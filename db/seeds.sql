c75bbd5e0d8e279f020bf7a25a5d4d62415b5057
Liz Ireland <liz.t.ireland@gmail.com>
2021-11-27 21:40:35 -0800

    updating seeds.sql doc

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,27 @@
-INSERT INTO students (first_name, last_name, enrolled)
-VALUES ("Elliot", "Smith", false),
-       ("Amira", "Afzal", true),
-       ("Christoper", "Lee", true),
-       ("Veronica", "Rodriguez", false),
-       ("Igor", "Ivanov", true);
-       
+INSERT INTO department (name)
+VALUES
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Sales Lead", 100000, 1),
+    ("Sales Person", 70000, 1),
+    ("Lead Engineer", 100000, 2),
+    ("Junior Engineer", 70000, 2),
+    ("Account Manager", 100000, 3),
+    ("Accountant", 80000, 3),
+    ("Legal Team Lead", 100000, 4),
+    ("Lawyer", 100000, 4);
+    
+ INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ VALUES
+    ("Mark", "Mothersbaugh", 1, NULL),
+    ("Bob", "Mothersbaugh", 2, 1),
+    ("Gerald", "Casale", 3, NULL),
+    ("Bob", "Casale", 4, 3),
+    ("Wendy", "OWilliams", 5, NULL),
+    ("Siouxsie", "Sioux", 6, 5),
+    ("Gaye", "Advert", 7, NULL),
+    ("Joe", "Strummer", 8, 7);
