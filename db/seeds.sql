e01e57af59500a80c604bf072d53c13054bebbd4
Liz Ireland <liz.t.ireland@gmail.com>
2021-11-27 21:53:51 -0800

    adding department ID to seeds.sql doc

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,13 +15,13 @@ VALUES
     ("Legal Team Lead", 100000, 4),
     ("Lawyer", 100000, 4);
     
- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ INSERT INTO employee (first_name, last_name, role_id, manager_id, department_id)
  VALUES
-    ("Mark", "Mothersbaugh", 1, NULL),
-    ("Bob", "Mothersbaugh", 2, 1),
-    ("Gerald", "Casale", 3, NULL),
-    ("Bob", "Casale", 4, 3),
-    ("Wendy", "OWilliams", 5, NULL),
-    ("Siouxsie", "Sioux", 6, 5),
-    ("Gaye", "Advert", 7, NULL),
-    ("Joe", "Strummer", 8, 7);
+    ("Mark", "Mothersbaugh", 1, NULL, 1),
+    ("Bob", "Mothersbaugh", 2, 1, 1),
+    ("Gerald", "Casale", 3, NULL, 2),
+    ("Bob", "Casale", 4, 3, 2),
+    ("Wendy", "OWilliams", 5, NULL, 3),
+    ("Siouxsie", "Sioux", 6, 5, 3),
+    ("Gaye", "Advert", 7, NULL, 4),
+    ("Joe", "Strummer", 8, 7, 4);
