89fcbdf02a0dbf03639ee4ec6c5cd609a067e13d
Liz Ireland <liz.t.ireland@gmail.com>
2021-11-28 16:10:28 -0800

    updating questins and inquirer function

--- app.js
+++ app.js
@@ -17,43 +17,105 @@ const db = mysql.createConnection(
   db.connect (err => {
       if (err) throw err
       console.log("farts");
+      allQuestions ()
   })
   // Query database
 //   db.query('SELECT * FROM students', function (err, results) {
 //     console.log(results);
 //   });
 
-  function promptUser () {
-      inquirer.prompt([
-          {
-              type: "list",
-              message: "What do you want to do?",
-              name: "option",
-              choices: [ 
-                "view all departments",
-                "view all roles",
-                "view all employees", 
-                "add a department", 
-                "add a role", 
-                "add an employee", 
-                "update an employee role"]
-          }
-      ]) .then (function(data){
-        console.log(data)
-      })
+  // function promptUser () {
+  //     inquirer.prompt([
+  //         {
+  //             type: "list",
+  //             message: "What do you want to do?",
+  //             name: "option",
+  //             choices: [ 
+  //               "view all departments",
+  //               "view all roles",
+  //               "view all employees", 
+  //               "add a department", 
+  //               "add a role", 
+  //               "add an employee", 
+  //               "update an employee role"]
+  //         }
+  //     ]) .then (function(data){
+  //       console.log(data)
+  //     })
+  // }
+
+  menu = [
+    {
+        type: 'rawlist',
+        message: "Where Would You Like to Go?",
+        name: "answer",
+        choices: [
+            "View All Departments",
+            "View All Roles?",
+            "View All Employees?",
+            "Add a Department?",
+            "Add a Role?",
+            "Add an Employee?",
+            "Update an Employee",
+            "Exit the Database"
+        ]
+    },
+];
+
+const promptUser = () => {
+    return inquirer.prompt(menu)
+}
+
+  function allQuestions () {
+    console.log("shit")
+    promptUser ()
+      .then (({ answer }) => {
+        switch (answer) {
+            case "view all departments":
+              viewAllDepartments();
   }
 
   function viewAllDepartments () {
     console.log("All Departments")
-    connection.promise().query("SELECT id, name FROM department;")
-        .then(result => {
-            console.table(result[0]);
-            mainMenu();
-        })
+    allQuestions ()
   }
 
-  function allQuestions () {
-    promptUser()
-    
-  }
-  allQuestions ();
+  function createDepartment() {
+    console.log("Create New Department")
+    inquirer.prompt([
+        {
+            name: "name",
+            message: "What is the name of the department?"
+        }
+    ]).then(input => {
+        let name = input;
+        connection.promise().query("INSERT INTO department SET ?", name)
+            .then(() => console.log(`Added ${name.name} to the database`))
+            .then(() => promptUser())
+    })
+}
+
+function viewAllRoles () {
+  console.log("All Roles")
+  connection.promise().query("SELECT id, name FROM role;")
+      .then(result => {
+          console.table(result[0]);
+          promptUser();
+      })
+}
+
+function createRole() {
+  console.log("Create New Role")
+  inquirer.prompt([
+      {
+          name: "name",
+          message: "What is the name of the role?"
+      }
+  ]).then(input => {
+      let name = input;
+      connection.promise().query("INSERT INTO role SET ?", name)
+          .then(() => console.log(`Added ${name.name} to the database`))
+          .then(() => promptUser())
+  })
+}
+})}
